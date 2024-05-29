1b79bf6cfc6c0c6cc0ee3c16f1037d2405d49ac9
William Furr <william.m.furr@gmail.com>
2024-05-29 11:07:53 -0500

    Did some efficiency changes to Fortran code in fpi.f90

--- fpi.f90
+++ fpi.f90
@@ -1,46 +1,48 @@
+! Fortran module with two subroutines for estimating pi using Monte-Carlo method. Intended for 
+! compilation as a module to import and use in Python. Uses C-types and bind() to allow for 
+! direct import and use in Python with Python built-in ctypes module.
 module fpi
     use, intrinsic :: iso_c_binding, only: rk => c_double, ik => c_int32_t
     implicit none
     contains
 
-        ! Subroutine one uses a do-loop
+        ! Subroutine  for estimating pi using a single do-loop
         subroutine dofpi(n, cpi) bind(C, name='dofpi')
             integer(ik), intent(in) :: n
             real(rk), intent(out) :: cpi
             integer(ik) :: i
-            real(rk) :: x, y, r
+            real(rk) :: x, y, r 
             real(rk) :: n_circle, n_square
             
             ! initialize the number of points in the circle and the number in the square to zero
-            n_circle = 0.0
-            n_square = 0.0
+            n_circle = 0_rk
+            n_square = 0_rk
             
             ! loop over the number of iterations
-            do i = 1, n
+            do i=1, n
                 ! generate random value for x and y on [0.0, 1.0)
                 call random_number(x)
                 call random_number(y)
 
                 ! calculate the radius
-                r = x * x + y * y
+                r = x*x + y*y
 
                 ! any less than or equal to 1 are in the circle
-                if (r<=1) then
-                    n_circle = n_circle + 1
-
+                if (r<=1_rk) then
+                    n_circle = n_circle + 1_rk
                 endif
 
                 ! all fall in the square
-                n_square = n_square + 1
+                n_square = n
                 
             enddo
 
             ! estimate pi
-            cpi = (4 * (n_circle / n_square))
+            cpi = 4_rk * (n_circle / n_square)
             
         end subroutine
 
-        ! Subroutine two uses vectorized array broadcasting
+        ! Subroutine  for estimating pi using vectorized array broadcasting
         subroutine vfpi(n, cpi) bind(C, name='vfpi')
             integer(ik), intent(in) :: n
             real(rk), intent(out) :: cpi
@@ -48,28 +50,25 @@ module fpi
             real(rk) :: n_circle, n_square
             
             ! initialize the number of points in the circle and the number in the square to zero
-            n_circle = 0.0
-            n_square = 0.0
+            n_circle = 0_rk
+            n_square = 0_rk
             
             ! generate random value for x and y on [0.0, 1.0) over the entire array x and y
             call random_number(x)
             call random_number(y)
 
             ! calculate the radius
-            r = x * x + y * y
+            r = x*x + y*y
 
             ! any less than or equal to one are in the circle
-            n_circle = count(r<=1)
+            n_circle = count(r<=1_rk)
 
             ! all fall in the square
             n_square = n
 
             ! estimate pi            
-            cpi = (4 * (n_circle / n_square))
+            cpi = 4_rk * (n_circle / n_square)
 
         end subroutine
-
-
-
 
 end module 
