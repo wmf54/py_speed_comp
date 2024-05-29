d09798344c16a089139ca0ab95c7af27202485a0
William Furr <william.m.furr@gmail.com>
2024-05-29 08:11:58 -0500

    Added explicit c-interface for the subroutines used in fpi.f90

--- fpi.f90
+++ fpi.f90
@@ -1,14 +1,15 @@
 module fpi
+    use, intrinsic :: iso_c_binding, only: rk => c_double, ik => c_int32_t
     implicit none
     contains
 
         ! Subroutine one uses a do-loop
-        subroutine dofpi(n, cpi)
-            intent(in) n
-            intent(out) cpi
-            integer :: n, i
-            double precision :: x, y, r
-            double precision :: n_circle, n_square, cpi
+        subroutine dofpi(n, cpi) bind(C, name='dofpi')
+            integer(ik), intent(in) :: n
+            real(rk), intent(out) :: cpi
+            integer(ik) :: i
+            real(rk) :: x, y, r
+            real(rk) :: n_circle, n_square
             
             ! initialize the number of points in the circle and the number in the square to zero
             n_circle = 0.0
@@ -24,27 +25,27 @@ module fpi
                 r = x * x + y * y
 
                 ! any less than or equal to 1 are in the circle
-                if (r .le.  1) then
+                if (r<=1) then
                     n_circle = n_circle + 1
 
-                end if
+                endif
 
                 ! all fall in the square
                 n_square = n_square + 1
-
-            end do
+                
+            enddo
 
             ! estimate pi
             cpi = (4 * (n_circle / n_square))
+            
         end subroutine
 
         ! Subroutine two uses vectorized array broadcasting
-        subroutine vfpi(n, cpi)
-            intent(in) n
-            intent(out) cpi
-            integer :: n
-            double precision, dimension(n) :: x, y, r
-            double precision :: n_circle, n_square, cpi
+        subroutine vfpi(n, cpi) bind(C, name='vfpi')
+            integer(ik), intent(in) :: n
+            real(rk), intent(out) :: cpi
+            real(rk), dimension(n) :: x, y, r
+            real(rk) :: n_circle, n_square
             
             ! initialize the number of points in the circle and the number in the square to zero
             n_circle = 0.0
